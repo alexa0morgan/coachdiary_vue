36cd6b93d05ef2277994961c45329914305da835
alexa0morgan <dasha2510929@yandex.ru>
2024-03-31 17:54:28 +0700

    добавление цветовой схемы по умолчанию

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import './assets/main.css'
 
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import {createApp} from 'vue'
+import {createPinia} from 'pinia'
 
 import App from './App.vue'
 import router from './router'
@@ -11,7 +11,58 @@ import {createVuetify} from "vuetify";
 import * as components from "vuetify/components"
 import * as directives from "vuetify/directives"
 
-const vuetify = createVuetify({components, directives})
+const myCustomLightTheme = {
+    dark: false,
+    colors: {
+        background: '#FFFFFF',
+        surface: '#F3EDF7',
+        'surface-bright': '#FFFFFF',
+        'surface-light': '#EEEEEE',
+        'surface-variant': '#424242',
+        'on-surface-variant': '#EEEEEE',
+        primary: '#6750A4',
+        'primary-darken-1': '#1F5592',
+        secondary: '#48A9A6',
+        'secondary-darken-1': '#018786',
+        error: '#B00020',
+        info: '#2196F3',
+        success: '#4CAF50',
+        warning: '#FB8C00',
+    },
+    variables: {
+        'border-color': '#000000',
+        'border-opacity': 0.12,
+        'high-emphasis-opacity': 0.87,
+        'medium-emphasis-opacity': 0.60,
+        'disabled-opacity': 0.38,
+        'idle-opacity': 0.04,
+        'hover-opacity': 0.04,
+        'focus-opacity': 0.12,
+        'selected-opacity': 0.08,
+        'activated-opacity': 0.12,
+        'pressed-opacity': 0.12,
+        'dragged-opacity': 0.08,
+        'theme-kbd': '#212529',
+        'theme-on-kbd': '#FFFFFF',
+        'theme-code': '#F5F5F5',
+        'theme-on-code': '#000000',
+    }
+}
+
+const vuetify = createVuetify({
+    components,
+    directives,
+    defaults: {
+        VBtn: {
+            variant: "flat",
+            class: "font-weight-bold text-none",
+            color: "primary"
+        }
+    },
+    theme: {
+        defaultTheme: "myCustomLightTheme",
+        themes: {myCustomLightTheme}}
+})
 
 const app = createApp(App)
 
