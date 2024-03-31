81c7b2d18d4aec477c582819dc8484756fb74d66
alexa0morgan <dasha2510929@yandex.ru>
2024-03-31 17:55:22 +0700

    Создание компонента app bar'а

---
+++ src/components/AppBar.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <v-app-bar :elevation="2">
+    <v-app-bar-title>Дневник тренера</v-app-bar-title>
+
+    <template #append>
+      <v-btn variant="text" href="#AboutSite">О сайте</v-btn>
+      <v-btn variant="text">Возможности</v-btn>
+      <v-btn variant="text">О нас</v-btn>
+      <v-btn variant="flat" rounded>Войти</v-btn>
+    </template>
+  </v-app-bar>
+
+</template>
+
+<style scoped>
+
+</style>
