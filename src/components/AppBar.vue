5f053fe4954651db03757c153b9304ed1801e49a
alexa0morgan <dasha2510929@yandex.ru>
2024-04-03 23:35:16 +0700

    Создание файла страницы входа

--- src/components/AppBar.vue
+++ src/components/AppBar.vue
@@ -1,16 +1,18 @@
 <script setup lang="ts">
-
+import { ref } from 'vue'
+import { useDisplay } from 'vuetify'
+const display = ref(useDisplay())
 </script>
 
 <template>
   <v-app-bar :elevation="2">
-    <v-app-bar-title>Дневник тренера</v-app-bar-title>
+    <v-app-bar-title v-if="display.smAndUp">Дневник тренера</v-app-bar-title>
 
     <template #append>
-      <v-btn variant="text" href="#AboutSite">О сайте</v-btn>
-      <v-btn variant="text" href="#Capabilities">Возможности</v-btn>
-      <v-btn variant="text" href="#AboutUs">О нас</v-btn>
-      <v-btn variant="flat" rounded>Войти</v-btn>
+      <v-btn variant="text" href="/#AboutSite">О сайте</v-btn>
+      <v-btn variant="text" href="/#Capabilities">Возможности</v-btn>
+      <v-btn variant="text" href="/#AboutUs">О нас</v-btn>
+      <v-btn variant="flat" :to="{name: 'login'}" rounded>Войти</v-btn>
     </template>
   </v-app-bar>
 
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import LoginPage from '../views/LoginPage.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -9,6 +10,11 @@ const router = createRouter({
       name: 'home',
       component: HomeView
     },
+    {
+      path: '/login',
+      name: 'login',
+      component: LoginPage
+    }
   ]
 })
 
