37c96b15335eb871a8ac840e5bb39632e2d26d4b
alexa0morgan <dasha2510929@yandex.ru>
2024-04-11 18:40:41 +0700

    Создание компонента таблицы для страницы Мои Нормативы

---
+++ src/components/StandardsTable.vue
@@ -0,0 +1,80 @@
+<!--suppress CssUnresolvedCustomProperty -->
+
+<script setup lang="ts">
+const props = defineProps<{
+  gender: 'm' | 'f';
+  high: number;
+  middle: number;
+  low: number;
+}>()
+</script>
+
+<template>
+  <div class="gender-text">{{ gender === 'm' ? 'Мальчики' : 'Девочки' }}</div>
+  <table class="table">
+    <thead>
+    <tr>
+      <td>Ступень</td>
+      <td>Норма</td>
+    </tr>
+    </thead>
+    <tbody>
+    <tr>
+      <td>Повышенная</td>
+      <td>{{ high }}</td>
+    </tr>
+    <tr>
+      <td>Средняя</td>
+      <td>{{ middle }}</td>
+    </tr>
+    <tr>
+      <td>Низкая</td>
+      <td>{{ low }}</td>
+    </tr>
+    </tbody>
+  </table>
+</template>
+
+<style scoped>
+.gender-text {
+  text-transform: uppercase;
+  font-weight: bold;
+  margin-left: 12px;
+  font-size: 20px;
+}
+
+.table {
+  width: 100%;
+  border-spacing: 0;
+  border-radius: 10px;
+  overflow: hidden;
+  font-weight: bold;
+
+  thead tr {
+    text-transform: uppercase;
+    font-weight: bold;
+    background: rgb(var(--v-theme-surface));
+
+    td {
+      padding: 12px 40px;
+    }
+  }
+
+  tbody {
+    color: rgb(var(--v-theme-primary));
+
+    td {
+      padding: 4px 40px;
+    }
+
+    tr:nth-child(even) {
+      background: rgb(var(--v-theme-surface));
+    }
+
+
+  }
+
+}
+
+
+</style>
