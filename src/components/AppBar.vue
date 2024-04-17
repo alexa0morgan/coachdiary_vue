e60cff48451c11d28d47acda22c59aeb1f6160d7
alexa0morgan <dasha2510929@yandex.ru>
2024-04-17 21:08:15 +0700

    Добавила изменение appbar'а на страницах самого приложения

--- src/components/AppBar.vue
+++ src/components/AppBar.vue
@@ -1,3 +1,4 @@
+<!--suppress CssUnresolvedCustomProperty -->
 <script setup lang="ts">
 
 import { useDisplay } from 'vuetify'
@@ -9,32 +10,76 @@ const isMenuOpen = ref(false)
 const route = useRoute()
 const isLoginPage = computed(() => route.name === 'login')
 
+const isLoggedInView = computed(() => route.path.startsWith('/app'))
+const logoLink = computed(() => {
+  if (isLoggedInView.value) return { name: 'my-classes' }
+  return { name: 'home' }
+})
+
+const plusButtonLink = computed(() => {
+  if (route.name === 'my-classes') {
+    return { name: 'home' }
+  } else if (route.name === 'my-standards') {
+    return { name: 'home' }
+  }
+  return ''
+})
+
+
+const props = defineProps<{
+  mobileTitle?: string;
+}>()
 </script>
 
 <template>
   <v-navigation-drawer v-model="isMenuOpen" v-if="!smAndUp">
-    <v-list-item title="Дневник Тренера" :to="{name: 'home'}" />
+    <v-list-item title="Дневник Тренера" :to="logoLink" />
     <v-divider />
-    <v-list-item link href="/#AboutSite" title="О сайте" />
-    <v-list-item link href="/#Capabilities" title="Возможности" />
-    <v-list-item link href="/#AboutUs" title="О нас" />
+    <template v-if="!isLoggedInView">
+      <v-list-item link href="/#AboutSite" title="О сайте" />
+      <v-list-item link href="/#Capabilities" title="Возможности" />
+      <v-list-item link href="/#AboutUs" title="О нас" />
+    </template>
+    <template v-else>
+      <v-list-item link :to="{name: 'my-classes'}" title="Мои классы" />
+      <v-list-item link :to="{name: 'my-standards'}" title="Мои нормативы" />
+      <v-list-item link :to="{name: 'profile'}" title="Профиль" />
+    </template>
   </v-navigation-drawer>
 
   <v-app-bar :elevation="2">
     <v-app-bar-title>
-      <router-link v-if="smAndUp" class="title" :to="{name: 'home'}">Дневник тренера</router-link>
+      <router-link v-if="smAndUp" class="title" :to="logoLink">Дневник тренера</router-link>
+      <div v-else class="text-center mr-4 mobile-title">{{ mobileTitle }}</div>
     </v-app-bar-title>
+
     <template #prepend>
       <v-btn v-if="!smAndUp" icon="mdi-menu" @click="isMenuOpen = true" />
     </template>
+
     <template #append>
-      <template v-if="smAndUp">
-        <v-btn variant="text" href="/#AboutSite">О сайте</v-btn>
-        <v-btn variant="text" href="/#Capabilities">Возможности</v-btn>
-        <v-btn variant="text" href="/#AboutUs">О нас</v-btn>
+
+      <template v-if="!isLoggedInView">
+
+        <template v-if="smAndUp">
+          <v-btn variant="text" href="/#AboutSite">О сайте</v-btn>
+          <v-btn variant="text" href="/#Capabilities">Возможности</v-btn>
+          <v-btn variant="text" href="/#AboutUs">О нас</v-btn>
+        </template>
+
+        <v-btn v-if="!isLoginPage" variant="flat" :to="{name: 'login'}" rounded>Войти</v-btn>
       </template>
 
-      <v-btn v-if="!isLoginPage" variant="flat" :to="{name: 'login'}" rounded>Войти</v-btn>
+      <template v-else-if="smAndUp">
+        <v-btn variant="text" :to="{name: 'my-classes'}">Мои классы</v-btn>
+        <v-btn variant="text" :to="{name: 'my-standards'}">Мои нормативы</v-btn>
+        <v-btn variant="flat" :to="{name: 'profile'}" rounded>Профиль</v-btn>
+      </template>
+
+      <template v-else-if="!smAndUp && plusButtonLink">
+        <v-btn variant="outlined" icon="mdi-plus" size="small" :to="plusButtonLink"/>
+      </template>
+
     </template>
   </v-app-bar>
 </template>
@@ -43,5 +88,10 @@ const isLoginPage = computed(() => route.name === 'login')
 .title {
   color: black;
   text-decoration: none;
+}
+
+.mobile-title {
+  color: rgb(var(--v-theme-primary));
+  font-weight: bold;
 }
 </style>
