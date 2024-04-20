e1b5bb3319d41ef15758c3da5f8e9cb82228b92c
alexa0morgan <dasha2510929@yandex.ru>
2024-04-20 21:47:29 +0700

    Изменение компонента DataTableSideNav. Добавление изменения отображения для Умений

--- src/components/DataTableSideNav.vue
+++ src/components/DataTableSideNav.vue
@@ -1,6 +1,6 @@
 <!--suppress CssUnresolvedCustomProperty -->
 <script setup lang="ts">
-import { ref } from 'vue'
+import { ref, watch } from 'vue'
 
 const props = defineProps<{
   title: string;
@@ -8,6 +8,7 @@ const props = defineProps<{
     label: string;
     id: number;
   }[];
+  isPageTypeSkills?: boolean;
 }>()
 
 const emit = defineEmits<{
@@ -16,18 +17,22 @@ const emit = defineEmits<{
   delete: [id: number, inAllLevels: boolean];
 }>()
 
-const selectedId = ref(-1)
+const selectedId = ref(props.data[0]?.id ?? -1)
 
 function onSelect(id: number): void {
   selectedId.value = id
   emit('select', id)
 }
+
+watch(() => props.data, () => {
+  selectedId.value = 1
+})
 </script>
 
 <template>
   <div class="container">
     <div class="title">{{ title }}</div>
-    <div class="data">
+    <div class="data" :class="{'grid': isPageTypeSkills}">
       <v-btn v-for="item in data" :key="item.id" class="button" :text="item.label" variant="outlined" size="small"
              block @click="onSelect(item.id)" :active="selectedId === item.id" :ripple="false" />
     </div>
@@ -38,7 +43,7 @@ function onSelect(id: number): void {
         Удалить
         <v-menu activator="parent">
           <v-list density="compact">
-            <v-list-item @click="emit('delete', selectedId, false)" >
+            <v-list-item @click="emit('delete', selectedId, false)">
               <v-list-item-title>у уровня</v-list-item-title>
             </v-list-item>
             <v-list-item @click="emit('delete', selectedId, true)">
@@ -56,9 +61,7 @@ function onSelect(id: number): void {
 
 <style scoped>
 .container {
-  height: 500px;
-  width: 230px;
-  margin: 10px auto;
+  height: calc(100dvh - 230px);
 
   display: grid;
   grid-template-rows: auto 1fr auto;
@@ -77,6 +80,7 @@ function onSelect(id: number): void {
   border-bottom: 1px solid rgb(var(--v-another-surface));
 }
 
+
 .data {
   display: flex;
   flex-direction: column;
@@ -87,11 +91,28 @@ function onSelect(id: number): void {
   scrollbar-width: thin;
 }
 
+.grid {
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(200px, calc(33.33% - 20px)));
+  grid-auto-rows: min-content;
+  align-items: start;
+  justify-content: center;
+  gap: 20px;
+}
+
+
 .button {
   border-radius: var(--v-border-radius);
   font-size: 16px;
   flex-grow: 0;
+  height: auto !important;
+  min-height: 24px;
 }
+
+.button:deep(.v-btn__content) {
+  white-space: normal !important;
+}
+
 
 .button.v-btn--active {
   background-color: rgb(var(--v-theme-surface));
@@ -109,6 +130,4 @@ function onSelect(id: number): void {
   border-width: 2px;
   font-size: 12px;
 }
-
-
 </style>
