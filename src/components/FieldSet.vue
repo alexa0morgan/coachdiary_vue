1d5da57b679e96020882fea0f7766d2db4dcfdca
alexa0morgan <dasha2510929@yandex.ru>
2024-04-21 00:32:00 +0700

    Создание компонента FieldSet

---
+++ src/components/FieldSet.vue
@@ -0,0 +1,24 @@
+<!--suppress CssUnresolvedCustomProperty -->
+<script setup lang="ts">
+const props = defineProps<{
+  title: string
+}>()
+</script>
+
+<template>
+  <fieldset class="container">
+    <legend>{{ title }}</legend>
+    <slot></slot>
+  </fieldset>
+</template>
+
+<style scoped>
+.container {
+  border-radius: var(--v-border-radius);
+  padding: 5px 10px 7px;
+}
+legend {
+  margin-left: 8px;
+  padding: 0 5px;
+}
+</style>
