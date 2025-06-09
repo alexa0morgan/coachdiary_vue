b2817c58d4f52e518bd9154090bdc09070378887
darkselia <darkselia@users.noreply.github.com>
2025-06-09 17:49:28 +0700

    style: enhance styling in ProfileView styling

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -1,14 +1,16 @@
 <script lang="ts" setup>
 import { computed, onMounted, ref } from 'vue';
-import { get, getErrorMessage, patch, post, put } from '@/utils';
+import { get, getErrorMessage, patch, post } from '@/utils';
 import { toast } from 'vue-sonner';
 import { useUserStore } from '@/stores/user';
 import router from '@/router';
 import LoadingOverlay from '@/components/LoadingOverlay.vue';
 import FieldSet from '@/components/FieldSet.vue';
+import { useDisplay } from 'vuetify';
 
+const { smAndUp } = useDisplay();
 const userStore = useUserStore();
-const pageType = ref<'personal-info' | 'security' | ''>('');
+const pageType = ref<'personal-info' | 'security' | 'data-reports'>('personal-info');
 const isLoading = ref(false);
 const isImporting = ref(false);
 const isEmailVerified = ref(true);
@@ -173,7 +175,7 @@ async function putPassword() {
       confirm_new_password: passwordConfirmation.value,
       current_password: password.value,
     };
-    const response = await put('/api/profile/change_password/', requestData);
+    const response = await patch('/api/profile/change_password/', requestData);
     if (response.ok) {
       password.value = '';
       newPassword.value = '';
@@ -209,13 +211,16 @@ async function exportData(type: 'xlsx' | 'json') {
     const response = await get(url, params);
     if (response.ok) {
       let blob = new Blob();
-      let filename = 'couchdiary-data';
+      let filename = 'coachdiary-data';
       if (type === 'json') {
         const data = await response.json();
         blob = new Blob([JSON.stringify(data)], { type: 'application/json' });
         filename += '.json';
       } else if (type === 'xlsx') {
         blob = await response.blob();
+        if (exportIncludesNorms.value) filename += '-все-нормативы';
+        if (exportIncludesResults.value) filename += '-результаты';
+        if (exportIncludesStandards.value) filename += '-листы-нормативов';
         filename += '.xlsx';
       }
       const url = URL.createObjectURL(blob);
@@ -280,190 +285,238 @@ onMounted(async () => {
 </script>
 
 <template>
+  <div v-if="smAndUp" class="menu">
+    <v-btn
+      :variant="pageType === 'personal-info' ? 'tonal' : 'text'"
+      @click="pageType = 'personal-info'"
+    >
+      <v-icon class="mr-2">mdi-account</v-icon>
+      Личные данные
+    </v-btn>
+    <v-btn :variant="pageType === 'security' ? 'tonal' : 'text'" @click="pageType = 'security'">
+      <v-icon class="mr-2">mdi-lock</v-icon>
+      Безопасность
+    </v-btn>
+    <v-btn
+      :variant="pageType === 'data-reports' ? 'tonal' : 'text'"
+      @click="pageType = 'data-reports'"
+    >
+      <v-icon class="mr-2">mdi-database</v-icon>
+      Данные и отчеты
+    </v-btn>
+    <v-divider class="divider" color="rgb(var(--v-theme-primary-darken-1))" />
+    <v-btn color="error" :variant="'text'" @click="userStore.logout">
+      <v-icon class="mr-2">mdi-logout</v-icon>
+      Выйти из аккаунта
+    </v-btn>
+  </div>
+
+  <div v-else class="mobile-menu">
+    <v-select
+      v-model="pageType"
+      :items="[
+        { title: 'Личные данные', value: 'personal-info', icon: 'mdi-account' },
+        { title: 'Безопасность', value: 'security', icon: 'mdi-lock' },
+        { title: 'Данные и отчеты', value: 'data-reports', icon: 'mdi-database' },
+      ]"
+      item-title="title"
+      item-value="value"
+      variant="outlined"
+      density="default"
+      hide-details
+    >
+      <template #selection="{ item }">
+        <v-icon class="mr-2">{{ item?.raw?.icon }}</v-icon>
+        {{ item?.raw?.title }}
+      </template>
+      <template #item="{ props, item }">
+        <v-list-item v-bind="props">
+          <template #prepend>
+            <v-icon>{{ item?.raw?.icon }}</v-icon>
+          </template>
+        </v-list-item>
+      </template>
+    </v-select>
+  </div>
+
   <div class="main">
-    <div class="container rounded-lg">
-      <div class="title">
-        <v-icon color="primary">mdi-account</v-icon>
-        {{ userStore.isStudent ? 'Ученик' : 'Учитель' }}
-      </div>
-      <form class="text-field mb-4" @submit.prevent="patchName">
-        <div class="text-field-fio">
-          <v-text-field
-            v-model="firstName"
-            :readonly="userStore.isStudent"
-            :disabled="isLoading"
-            :clearable="!userStore.isStudent"
-            persistent-clear
-            label="Имя"
-          />
-          <v-text-field
-            v-model="lastName"
-            :disabled="isLoading"
-            :readonly="userStore.isStudent"
-            :clearable="!userStore.isStudent"
-            persistent-clear
-            label="Фамилия"
-          />
-          <v-text-field
-            v-model="patronymic"
-            :disabled="isLoading"
-            :readonly="userStore.isStudent"
-            :clearable="!userStore.isStudent"
-            persistent-clear
-            label="Отчество"
-          />
-        </div>
-        <v-btn
-          v-if="!userStore.isStudent"
-          :disabled="isSetNameButtonDisabled"
-          class="button"
-          rounded
-          text="Изменить"
-          type="submit"
-        />
-      </form>
-      <form class="text-field" @submit.prevent="patchEmail">
-        <v-text-field
-          v-model="email"
-          :disabled="isLoading"
-          clearable
-          persistent-clear
-          label="Почта"
-          type="email"
-        />
-        <div class="text-field-email">
-          <div v-if="!isEmailVerified" class="verify-email-block">
-            <span class="verify-email-text red">Почта не подтверждена</span>
-            <v-btn
-              variant="text"
-              size="small"
-              text="Отправить письмо повторно"
-              class="button-email"
-              @click="resendVerificationEmail"
+    <template v-if="pageType === 'personal-info'">
+      <div class="container rounded-lg">
+        <div class="title">Аккаунт {{ userStore.isStudent ? 'ученика' : 'учителя' }}</div>
+        <form class="text-field mb-4" @submit.prevent="patchName">
+          <div class="text-field-fio">
+            <v-text-field
+              v-model="firstName"
+              :readonly="userStore.isStudent"
+              :disabled="isLoading"
+              :clearable="!userStore.isStudent"
+              persistent-clear
+              label="Имя"
+            />
+            <v-text-field
+              v-model="lastName"
+              :disabled="isLoading"
+              :readonly="userStore.isStudent"
+              :clearable="!userStore.isStudent"
+              persistent-clear
+              label="Фамилия"
+            />
+            <v-text-field
+              v-model="patronymic"
+              :disabled="isLoading"
+              :readonly="userStore.isStudent"
+              :clearable="!userStore.isStudent"
+              persistent-clear
+              label="Отчество"
             />
           </div>
-          <span v-else class="verify-email-text green">Почта подтверждена</span>
           <v-btn
-            :disabled="isSetEmailButtonDisabled"
+            v-if="!userStore.isStudent"
+            :disabled="isSetNameButtonDisabled"
             class="button"
             rounded
             text="Изменить"
             type="submit"
           />
+        </form>
+        <form class="text-field" @submit.prevent="patchEmail">
+          <v-text-field
+            v-model="email"
+            :disabled="isLoading"
+            clearable
+            persistent-clear
+            label="Почта"
+            type="email"
+          />
+          <div class="text-field-email">
+            <div v-if="!isEmailVerified" class="verify-email-block">
+              <span class="verify-email-text red">Почта не подтверждена</span>
+              <v-btn
+                variant="text"
+                size="small"
+                text="Отправить письмо повторно"
+                class="button-email"
+                @click="resendVerificationEmail"
+              />
+            </div>
+            <span v-else class="verify-email-text green">Почта подтверждена</span>
+            <v-btn
+              :disabled="isSetEmailButtonDisabled"
+              class="button"
+              rounded
+              text="Изменить"
+              type="submit"
+            />
+          </div>
+        </form>
+      </div>
+    </template>
+
+    <template v-if="pageType === 'security'">
+      <div class="container rounded-lg">
+        <div class="title">Смена пароля</div>
+        <div class="text-red text">
+          Внимание. После смены пароля
+          <br />
+          необходимо будет снова войти в аккаунт
         </div>
-      </form>
-    </div>
-    <div class="container rounded-lg">
-      <div class="title">
-        <v-icon color="primary">mdi-lock</v-icon>
-        Смена пароля
-      </div>
-      <div class="text-red text">
-        Внимание. После смены пароля
-        <br />
-        необходимо будет снова войти в аккаунт
-      </div>
-      <form class="text-field" @submit.prevent="putPassword">
-        <v-text-field
-          v-model="password"
-          :disabled="isLoading"
-          :append-inner-icon="password ? 'mdi-eye' : undefined"
-          :type="passwordType"
-          clearable
-          label="Старый пароль"
-          persistent-clear
-          @click:append-inner="passwordType = passwordType === 'password' ? 'text' : 'password'"
-        />
-
-        <v-text-field
-          v-model="newPassword"
-          :disabled="isLoading"
-          :append-inner-icon="newPassword ? 'mdi-eye' : undefined"
-          :type="newPasswordType"
-          clearable
-          label="Новый пароль"
-          persistent-clear
-          @click:append-inner="
-            newPasswordType = newPasswordType === 'password' ? 'text' : 'password'
-          "
-        />
-        <v-text-field
-          v-model="passwordConfirmation"
-          :disabled="isLoading"
-          :append-inner-icon="passwordConfirmation ? 'mdi-eye' : undefined"
-          :type="passwordConfirmationType"
-          clearable
-          label="Проверка пароля"
-          persistent-clear
-          @click:append-inner="
-            passwordConfirmationType = passwordConfirmationType === 'password' ? 'text' : 'password'
-          "
-        />
-        <v-btn
-          :disabled="isSetPasswordButtonDisabled"
-          class="button"
-          rounded
-          text="Изменить"
-          type="submit"
-        />
-      </form>
-    </div>
-    <div v-if="userStore.isTeacher" class="container rounded-lg">
-      <div class="title">
-        <v-icon class="mr-2" color="primary">mdi-database-export</v-icon>
-        Импорт и экспорт данных
-      </div>
-      <div class="text">
-        Вы можете экспортировать и импортировать все свои данные в формате JSON. Это может быть
-        полезно, если вы хотите перенести свои данные на другой аккаунт или поделиться ими с кем-то
-      </div>
-      <div class="exp-imp-buttons">
-        <v-btn color="primary" rounded @click="exportData('json')">
-          <v-icon left>mdi-download</v-icon>
-          Экспортировать
-        </v-btn>
-        <v-btn color="primary" rounded @click="importDataJSON">
-          <v-icon left>mdi-upload</v-icon>
-          Импортировать
-        </v-btn>
-      </div>
-    </div>
-    <div v-if="userStore.isTeacher" class="container rounded-lg">
-      <div class="title">
-        <v-icon class="mr-2" color="primary">mdi-database-export</v-icon>
-        Создать отчет
-      </div>
-      <div class="text">Вы можете экспортировать данные в формате XLSX.</div>
-      <div class="text-field">
-        <FieldSet title="Созданный отчет будет содержать:">
-          <v-checkbox v-model="exportIncludesNorms" label="Лист с таблицей нормативов" />
-          <v-checkbox
-            v-model="exportIncludesResults"
-            label="Сводный лист с итоговыми результатами"
+        <form class="text-field" @submit.prevent="putPassword">
+          <v-text-field
+            v-model="password"
+            :disabled="isLoading"
+            :append-inner-icon="password ? 'mdi-eye' : undefined"
+            :type="passwordType"
+            clearable
+            label="Старый пароль"
+            persistent-clear
+            @click:append-inner="passwordType = passwordType === 'password' ? 'text' : 'password'"
           />
-          <v-checkbox
-            v-model="exportIncludesStandards"
-            label="Отдельные листы для каждого норматива"
-          />
-        </FieldSet>
-        <v-btn
-          :disabled="isSetExportButtonDisabled"
-          color="primary"
-          class="button"
-          rounded
-          @click="exportData('xlsx')"
-        >
-          <v-icon left>mdi-download</v-icon>
-          Скачать отчет
-        </v-btn>
-      </div>
-    </div>
 
-    <div class="container rounded-lg">
-      <div class="title">Выйти из аккаунта</div>
-      <v-btn rounded variant="flat" @click="userStore.logout">Выйти</v-btn>
-    </div>
+          <v-text-field
+            v-model="newPassword"
+            :disabled="isLoading"
+            :append-inner-icon="newPassword ? 'mdi-eye' : undefined"
+            :type="newPasswordType"
+            clearable
+            label="Новый пароль"
+            persistent-clear
+            @click:append-inner="
+              newPasswordType = newPasswordType === 'password' ? 'text' : 'password'
+            "
+          />
+          <v-text-field
+            v-model="passwordConfirmation"
+            :disabled="isLoading"
+            :append-inner-icon="passwordConfirmation ? 'mdi-eye' : undefined"
+            :type="passwordConfirmationType"
+            clearable
+            label="Проверка пароля"
+            persistent-clear
+            @click:append-inner="
+              passwordConfirmationType =
+                passwordConfirmationType === 'password' ? 'text' : 'password'
+            "
+          />
+          <v-btn
+            :disabled="isSetPasswordButtonDisabled"
+            class="button"
+            rounded
+            text="Изменить"
+            type="submit"
+          />
+        </form>
+      </div>
+    </template>
+
+    <template v-if="pageType === 'data-reports'">
+      <div v-if="userStore.isTeacher" class="container rounded-lg">
+        <div class="title">Создать отчет</div>
+        <div class="text">Вы можете экспортировать данные в формате XLSX.</div>
+        <div class="text-field">
+          <FieldSet title="Созданный отчет будет содержать:">
+            <v-checkbox v-model="exportIncludesNorms" label="Лист с таблицей нормативов" />
+            <v-checkbox
+              v-model="exportIncludesResults"
+              label="Сводный лист с итоговыми результатами"
+            />
+            <v-checkbox
+              v-model="exportIncludesStandards"
+              label="Отдельные листы для каждого норматива"
+            />
+          </FieldSet>
+          <v-btn
+            :disabled="isSetExportButtonDisabled"
+            color="primary"
+            class="button"
+            rounded
+            @click="exportData('xlsx')"
+          >
+            <v-icon left>mdi-download</v-icon>
+            Скачать отчет
+          </v-btn>
+        </div>
+      </div>
+      <div v-if="userStore.isTeacher" class="container rounded-lg">
+        <div class="title">Импорт и экспорт данных</div>
+        <div class="text">
+          Вы можете экспортировать и импортировать все свои данные в формате JSON. Это может быть
+          полезно, если вы хотите перенести свои данные на другой аккаунт или поделиться ими с
+          кем-то
+        </div>
+        <div class="exp-imp-buttons">
+          <v-btn color="primary" rounded @click="exportData('json')">
+            <v-icon left>mdi-download</v-icon>
+            Экспортировать
+          </v-btn>
+          <v-btn color="primary" rounded @click="importDataJSON">
+            <v-icon left>mdi-upload</v-icon>
+            Импортировать
+          </v-btn>
+        </div>
+      </div>
+    </template>
   </div>
+
   <LoadingOverlay v-model="isImporting" task="импорт данных" />
 </template>
 
@@ -473,7 +526,31 @@ onMounted(async () => {
   flex-direction: column;
   gap: 10px;
   align-items: center;
-  margin: 20px 0;
+  margin: 20px 10px 20px 260px;
+}
+
+.menu {
+  position: fixed;
+  top: 124px;
+  left: 10px;
+  z-index: 1000;
+  display: flex;
+  flex-direction: column;
+  align-items: start;
+  gap: 5px;
+  background-color: rgb(var(--v-theme-surface));
+  padding: 16px;
+  border-radius: 8px;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.1);
+}
+
+.divider {
+  width: calc(100% - 20px);
+  align-self: center;
+}
+
+.mobile-menu {
+  margin: 0 10px;
 }
 
 .container {
@@ -557,22 +634,33 @@ onMounted(async () => {
   justify-content: center;
 }
 
+@media (width <= 900px) {
+  .text-field-fio {
+    flex-direction: column;
+  }
+}
+
 @media (max-width: 800px) {
-  .main {
-    margin-top: 0;
+  .menu {
+    top: 85px;
   }
 
   .container {
-    background: transparent;
     padding: 20px;
-  }
-
-  .text-field-fio {
-    flex-direction: column;
   }
 
   .text {
     font-size: 18px;
+  }
+}
+
+@media (max-width: 600px) {
+  .main {
+    margin: 10px 20px;
+  }
+
+  .container {
+    background: transparent;
   }
 }
 </style>
